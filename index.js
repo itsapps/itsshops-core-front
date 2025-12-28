8a4774f46eefca3a4053e903363bbfd0f4e862c3
kampfgnu <dev@itsapps.at>
2025-12-28 22:31:24 +0100

    load more config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fs from "fs"
 import path from "path"
+import Nunjucks from "nunjucks"
 import { fileURLToPath } from "url"
 
 import priceData from "./shortcodes/priceData.js"
@@ -9,6 +10,17 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default function shopCoreFrontend(eleventyConfig) {
+  eleventyConfig.addPlugin(shopCoreFrontend)
+
+  let nunjucksEnvironment = new Nunjucks.Environment(
+		new Nunjucks.FileSystemLoader([
+      path.resolve("src/_includes"),
+      path.resolve("node_modules/@itsapps/itsshops-core-front"),
+    ], { noCache: true })
+	);
+	eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+
+  
   // eleventyConfig.addShortcode("priceData", priceData)
   eleventyConfig.addNunjucksGlobal("priceData", priceData)
 
@@ -18,4 +30,16 @@ export default function shopCoreFrontend(eleventyConfig) {
   const templateContent = fs.readFileSync(templatePath, "utf-8")
 
   eleventyConfig.addTemplate("something.njk", templateContent)
+
+  // return {
+  //   dir: {
+  //     input: 'src',
+  //     output: 'dist',
+  //     // includes: '_includes',
+  //     layouts: '_layouts'
+  //   },
+  //   dataTemplateEngine: 'njk',
+  //   htmlTemplateEngine: 'njk',
+  // };
+  
 }
