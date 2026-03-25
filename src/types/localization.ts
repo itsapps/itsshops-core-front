9c6b8446637200f887d934184d973c983828e320
kampfgnu <dev@itsapps.at>
2026-03-25 11:17:47 +0100

    redirects, maintenance, 404, some static pages (checkout, order thanks, some user related

--- src/types/localization.ts
+++ src/types/localization.ts
@@ -13,12 +13,13 @@ export interface ITSi18nDictValue<T = string> {
 export type LocalizedStringArray = Array<{ _key: string; value?: string }> | undefined
 
 export type PermalinkTranslations = {
-  product?:  string
-  category?: string
-  blog?:     string
-  checkout?: string
-  account?:  string
-  register?: string
-  recover?:  string
-  login?:    string
+  product?:     string
+  category?:    string
+  blog?:        string
+  checkout?:    string
+  orderThanks?: string
+  account?:     string
+  register?:    string
+  recover?:     string
+  login?:       string
 }
